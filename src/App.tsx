cc75408f0a49160e630c0dfdb57b33c3f29daf81
Bela Bohlender <bbohlender@coconut-capital.com>
2023-08-11 15:06:47 +0200

    enter and info dialog and fix lighting and upgrade dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,48 @@
 import { XRCanvas } from "@coconut-xr/natuerlich/defaults";
 import {
-  IncludeWhenInSessionMode,
   NonImmersiveCamera,
-  useEnterXR,
+  SessionModeGuard,
   useHeighestAvailableFrameRate,
   useNativeFramebufferScaling,
 } from "@coconut-xr/natuerlich/react";
 import { Environment } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import ImmersiveSession from "./components/ImmersiveSession";
+import { EnterDialog } from "./components/html/enter";
+import { SessionInfo } from "./components/html/info";
 import MusicPlayer from "./components/music-player/MusicPlayer";
 
 function App() {
-  const settings = { requiredFeatures: ["local-floor", "hand-tracking"] };
-  const enterAR = useEnterXR("immersive-ar", settings);
-  const enterVR = useEnterXR("immersive-vr", settings);
+  const [state, setState] = useState<"hide" | "enter-dialog" | "VR" | "AR">(
+    "enter-dialog",
+  );
 
   const frameBufferScaling = useNativeFramebufferScaling();
   const heighestAvailableFramerate = useHeighestAvailableFrameRate();
 
   return (
     <>
-      <div className="xr-buttons">
-        <button className="xr-button" onClick={enterAR}>
-          AR
-        </button>
-        <button className="xr-button" onClick={enterVR}>
-          VR
-        </button>
-      </div>
+      {state === "enter-dialog" && <EnterDialog setState={setState} />}
+      {(state === "VR" || state === "AR") && (
+        <SessionInfo type={state} setState={setState} />
+      )}
       <XRCanvas
         dpr={window.devicePixelRatio}
         gl={{ localClippingEnabled: true }}
         frameBufferScaling={frameBufferScaling}
         frameRate={heighestAvailableFramerate}
       >
-        <directionalLight position={[-2, 2, 2]} intensity={0.8} />
+        <directionalLight position={[-2, 2, 2]} intensity={1.6} />
         <MusicPlayer />
-        <IncludeWhenInSessionMode deny="immersive-ar">
+        <SessionModeGuard deny="immersive-ar">
           <Suspense>
-            <Environment blur={0.05} files="skybox/apartment_4k.hdr" background />
+            <Environment
+              blur={0.05}
+              files="skybox/apartment_4k.hdr"
+              background
+            />
           </Suspense>
-        </IncludeWhenInSessionMode>
+        </SessionModeGuard>
         <ImmersiveSession />
         <NonImmersiveCamera position={[0, 1.5, 0.5]} />
       </XRCanvas>
